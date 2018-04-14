3fb0a38175d2405f790c436ea52e28d5a122eac6
HeiLi <18991492@qq.com>
2018-04-14 10:34:40 +0800

    字符串移位

---
+++ src/com/hust/algorithm/string/cases/_0005StringShift.java
@@ -0,0 +1,30 @@
+package com.hust.algorithm.string.cases;
+
+import org.junit.Test;
+
+public class _0005StringShift {
+
+    public String shiftByIndex(String target, int index) {
+
+        _0005ReverseWords reverseWords = new _0005ReverseWords();
+
+        char[] chars = target.toCharArray();
+
+        int startIndex = 0;
+        int endIndex = target.length() - 1;
+
+        reverseWords.reverse(chars, startIndex, index);
+        reverseWords.reverse(chars, index + 1, endIndex);
+        reverseWords.reverse(chars, startIndex, endIndex);
+
+        return new String(chars);
+    }
+
+
+    @Test
+    public void testShiftByIndex() {
+
+        System.out.println(shiftByIndex("1234567890", 4));
+
+    }
+}
